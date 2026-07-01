8ce6a0a226ab92db7f82eef58d1c564b35c1f269
sreyapathi12 <sreyapathi12@gmail.com>
2026-07-01 09:37:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+sub1=int(input("enter sub1 marks:"))
+sub2=int(input("enter sub2 marks:"))
+sub3=int(input("enter sub3 marks:"))
+sub4=int(input("enter sub4 marks:"))
+sub5=int(input("enter sub5 marks:"))
+total=sub1+sub2+sub3+sub4+sub5
+percentage=total/5
+print("total of their marks is :", total)
+print("percentage of their marks is :",percentage)
